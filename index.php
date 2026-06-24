96e257dbb1ca1e9e352304887b378a88262aa20b
kari <iwanfadill37@gmail.com>
2026-06-24 10:33:13 +0800

    feat: adding index.php and routing into pages

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BELAU MART</title>
-</head>
-<body>
+<?php
+    // require_once'config/config.php';
     
-</body>
-</html>
+    $page = isset($_GET['page']) ? $_GET['page'] : 'home';
+
+    $allowed_pages = ['home', 'cart', 'profile', 'checkout', 'login', 'register'];
+    if (in_array($page, $allowed_pages)) {
+        $target_file = 'pages/' . $page . '.php';
+    } else {
+        $target_file = 'pages/404.php';
+    }
+
+    //include 'components/header.php';
+    include $target_file;
+    //include 'components/footer.php';
+
+?>
